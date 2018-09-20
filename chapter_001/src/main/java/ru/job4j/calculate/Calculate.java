1ebc318aefef57eba3f19d298b91209fc7a8187d
Daniil Ivanov <dania-spb@mail.ru>
2018-09-20 18:24:46 +0300

    1.3 JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j.calculate;
+/**
+ * Calculate.
+ * 
+ * @author Ivanov Daniil (dania-spb@mail.ru)
+ */
 public class Calculate {
+    /**
+     * Main.
+     * 
+     * @param args - args.
+     */
     public static void main(String[]args){
             System.out.println("Hello world!");
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ivanov Daniil (mailto:dania-spb@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
