c849f2f5e013863201a85e009078ad177e0011ee
Daniil Ivanov <dania-spb@mail.ru>
2018-10-12 10:47:08 +0200

    5.4. Построить пирамиду в псевдографике ver 1.0.1.

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -2,6 +2,8 @@ package ru.job4j.loop;
 
 import org.junit.Test;
 
+import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,6 +13,41 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
+
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
     @Test
     public void whenPaintPyWithHeightOfThreeThenPyWithHeightOfThree() {
         Paint paint = new Paint();
